153224723f10869f60cfe598f2845a79729fa543
Rogerio Damião <rogeriodominus@gmail.com>
2019-03-25 23:53:19 -0300

    Primeiros componentes com Angular

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro template com Angular</p>
+    `
+})
+export class MeuPrimeiroComponent{}
